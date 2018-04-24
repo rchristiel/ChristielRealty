3c7854e5d17ed8b22426e0ed13d94eb0d6b81672
Ramzee <rchristiel@hawk.iit.edu>
2018-04-23 19:55:49 -0500

    update

--- js/myscript.js
+++ js/myscript.js
@@ -1,40 +1,46 @@
-
 var map;
 var marker;
 var current;
+var chicago = {lat: 41.887855, lng: -87.630347};
+var Dubai = {lat: 25.2048, lng: 55.2708};
+var LosAngelas = {lat: 34.0522, lng: -118.2437};
+var chicago = {lat: 41.8781, lng:  -87.6298};
+var places = ["Chicago", "Dubai", "LosAngelas", "Chicago"];
 
-var chicago={lat: 41.8781, lng:  87.6298}
-var Dubai={lat: 25.2048, lng: 55.2708};
-var LosAngelas={lat: 34.0522, lng: 118.2437};
+function initMap() {
+  current = {lat: 41.887855, lng: -87.630347};
+  map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 10,
+  center: current
+  });
 
-var locations=["chicago", "Dubia"];
-
-function myMap() {
-	current={lat: 41.8781, lng: 87.6298};
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom:12,
-	center: chicago});
-	marker = new google.maps.Marker({
-		position:chicago,
-		animation:google.maps.Animation.DROP,
-	map: map });
+  marker = new google.maps.Marker({
+  position: current,
+  map: map
+  });
 }
 
-function travel(location, my_locations) {
-	map.panTo(location);
-	map.setZoom(15);
-	marker = new google.maps.Marker({
-		position: location,
-			animation:google.maps.Animation.BOUNCE,
-		map: map
-	});
-	if(my_locations==locations[0]){
-		document.getElementById("description").innerHTML="SOUTH CHICAGO"
-	}
-	else if(my_locations==locations[1]){
-		document.getElementById("description").innerHTML=""
-	}
-	else {
-		document.getElementById("description").innerHTML=null;
-	}
+function goto(location, title){
+  map.panTo(location);
+  map.setZoom(12);
+  marker = new google.maps.Marker({
+  position: location,
+  map: map
+  });
+  if (title==places[0]){
+    document.getElementById("demo").innerHTML = "Chicago, this has been home for all of my life.";
+    }
+  else if (title==places[1]){
+    document.getElementById("demo").innerHTML = "Dubai, I would love to travel, and posibly work here.";
+    }
+  else if (title==places[2]){
+    document.getElementById("demo").innerHTML = "LosAngelas, California in United States Home of Holly Wood";
+    }
+  else if (title==places[3]){
+    document.getElementById("demo").innerHTML = "Chicago, home of the Cubs.";
+    }
+  else
+    {
+    document.getElementById("demo").innerHTML = null;
+    }
 }
